5e3fa4de1461e6e9a5395771e97e45f1e833aaf4
mtacz <magdalena.taczuk+github@gmail.com>
2026-03-01 15:10:26 +0100

    ScreenShotMaker

--- src/test/java/page/ScreenShotPage.java
+++ src/test/java/page/ScreenShotPage.java
@@ -1,4 +1,73 @@
 package page;
 
-public class ScrennShotPage {
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.time.Duration;
+
+public class ScreenShotPage {
+
+    private final WebDriver driver;
+    private final WebDriverWait wait;
+    private final By searchBar = By.name("q");
+
+    public ScreenShotPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    }
+
+    public ScreenShotPage openPage() {
+        driver.get("https://duckduckgo.com/");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(searchBar));
+        return this;
+    }
+
+    public ScreenShotPage clickOnSearchBar() {
+        WebElement searchInput = waitForElement(searchBar);
+        searchInput.click();
+        return this;
+    }
+
+    public ScreenShotPage enterSearch(String query) {
+        WebElement searchInput = waitForElement(searchBar);
+        searchInput.clear();
+        searchInput.sendKeys(query);
+        return this;
+    }
+
+    public ScreenShotPage clickSubmit() {
+        WebElement searchInput = waitForElement(searchBar);
+        searchInput.submit();
+        return this;
+    }
+
+    public ScreenShotPage waitForLoading(String input) {
+        wait.until(ExpectedConditions.titleContains(input));
+        return this;
+    }
+
+    private WebElement waitForElement(By webElement) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(webElement));
+    }
+
+    public ScreenShotPage makeScreenshot(String fileName) {
+
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+        Path path = Paths.get("screenshot", fileName + ".png");
+        try {
+            Files.createDirectories(path.getParent());
+            Files.copy(scrFile.toPath(), path, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to save screenshot" + fileName, e);
+        }
+        return this;
+    }
 }
--- src/test/java/test/ScreenShotMakerTests.java
+++ src/test/java/test/ScreenShotMakerTests.java
@@ -1,4 +1,46 @@
 package test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import page.ScreenShotPage;
+
+import static org.testng.Assert.assertTrue;
+
 public class ScreenShotMakerTests {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void screenshotMakerTest() {
+        ScreenShotPage screenShotPage = new ScreenShotPage(driver);
+        String input = "Kurs Selenium";
+        screenShotPage.openPage()
+                .clickOnSearchBar()
+                .makeScreenshot("BeforeTypingQueryPage")
+                .enterSearch(input)
+                .makeScreenshot("AfterTypingQueryPage")
+                .clickSubmit()
+                .waitForLoading(input)
+                .makeScreenshot("SearchResultsPage");
+
+        assertTrue(driver.getTitle().contains(input));
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
